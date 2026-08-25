c7221adf16b10d66fdb3471837c1fd0dcd8a386b
kinvoedofrejus-cpu <kinvoedofrejus@gmail.com>
2026-08-25 08:08:04 +0100

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'kinvos-hub-v1';
+const CORE_ASSETS = ['/', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Réseau d'abord (le hub évolue souvent), avec repli sur le cache hors ligne.
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
